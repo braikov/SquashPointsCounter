ff98cf23ed171a457c866e43d15b658d06f5c4f2
Miroslav Braikov <miroslav.braikov@gmail.com>
2026-01-15 22:52:07 +0200

    Add tournament tabs partial and update page headers
    Introduce _TournamentTabs partial for consistent navigation on tournament pages, using new TournamentHeaderViewModel. Update page titles to show tournament name in DayMatches and Details views. Remove redundant card headers from Details.cshtml for cleaner layout.

---
+++ Squash.Web/Areas/Administration/Models/TournamentHeaderViewModel.cs
@@ -0,0 +1,9 @@
+namespace Squash.Web.Areas.Administration.Models
+{
+    public class TournamentHeaderViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ActiveTab { get; set; } = string.Empty;
+    }
+}
